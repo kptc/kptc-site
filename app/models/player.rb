d9b489730812771944ad3fdf80278d660b72e421
Barbara Goss <jaknoclistopadowa@gmail.com>
2015-04-04 14:59:47 -0400

    Changed email validation to strict; added custom validation messages

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,8 +2,8 @@ class Player < ActiveRecord::Base
   
   belongs_to :gender
   
-  validates :first_name, :last_name, :email, :phone, :gender_id, presence: true
-  validates :email, uniqueness: true, email: true
+  validates :first_name, :last_name, :email, :phone, :gender_id, :presence => {message: "Required"}
+  validates :email, :uniqueness => {message: "Email Already Taken"}, :email => {message: "Invalid Email"}
   
   def full_name
     [first_name, last_name].join(' ')
