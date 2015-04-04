ed9e3b12cbbd75249b22c69e2bf515451f863b5a
Barbara Goss <jaknoclistopadowa@gmail.com>
2015-04-04 14:46:07 -0400

    Added: email validator gem

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,8 +2,8 @@ class Player < ActiveRecord::Base
   
   belongs_to :gender
   
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, :last_name, :email, :phone, :gender_id, presence: true
+  validates :email, uniqueness: true, email: true
   
   def full_name
     [first_name, last_name].join(' ')
